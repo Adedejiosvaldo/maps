02fc07636c45560546b2f62e10d795b592133f8d
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-27 06:37:45 +0100

    Feature-Added Popup to marker

--- src/classes/Map.ts
+++ src/classes/Map.ts
@@ -16,12 +16,17 @@ export class customMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({ content: "Hello" });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
